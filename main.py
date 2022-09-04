1ed1ff8910343c721bdec1d3d8bf72448277ee19
Ben Tettmar <bentettmar@yahoo.com>
2022-09-04 21:18:41 +0100

    added numbers to tokens

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import string, random, base64
 
 if __name__ == "__main__":
-    random_string = lambda length: ''.join(random.choice(string.ascii_letters) for _ in range(length))
+    random_string = lambda length: ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(length))
     generate_token = lambda: base64.b64encode(str(random.randint(100000000000000000, 999999999999999999)).encode()).decode() + "." + random_string(5) + "." + random_string(38)
 
     amount = int(input("Amount to generate: "))
