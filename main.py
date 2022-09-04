2c064d562286b06b5223036abf6ffcfc36bab639
Ben Tettmar <83777519+bentettmar@users.noreply.github.com>
2022-09-04 21:25:22 +0100

    cool code bro

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import string, random, base64
 
-random_string = lambda length: ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(length))
-get_user_id = lambda: base64.b64encode(str(random.randint(100000000000000000, 999999999999999999)).encode()).decode()
+random_string  = lambda length: ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(length))
+get_user_id    = lambda: base64.b64encode(str(random.randint(100000000000000000, 999999999999999999)).encode()).decode()
 generate_token = lambda: get_user_id() + "." + random_string(5) + "." + random_string(38)
 
 if __name__ == "__main__":
-    amount = int(input("Amount to generate: "))
+    amount   = int(input("Amount to generate: "))
     accounts = [generate_token() for _ in range(amount)]
 
     if input("Save to file? (y/n): ") == "y":
