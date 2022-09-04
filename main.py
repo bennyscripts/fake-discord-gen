1f92e82f238dd5da8c50d86c8d77d041ec83acc3
Ben Tettmar <bentettmar@yahoo.com>
2022-09-04 21:19:48 +0100

    put functions outside if statement

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import string, random, base64
 
-if __name__ == "__main__":
-    random_string = lambda length: ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(length))
-    generate_token = lambda: base64.b64encode(str(random.randint(100000000000000000, 999999999999999999)).encode()).decode() + "." + random_string(5) + "." + random_string(38)
+random_string = lambda length: ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(length))
+generate_token = lambda: base64.b64encode(str(random.randint(100000000000000000, 999999999999999999)).encode()).decode() + "." + random_string(5) + "." + random_string(38)
 
+if __name__ == "__main__":
     amount = int(input("Amount to generate: "))
     accounts = [generate_token() for _ in range(amount)]
 
