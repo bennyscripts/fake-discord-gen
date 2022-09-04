49beb835d30e3ae0f006b00173894287353d9429
Ben Tettmar <bentettmar@yahoo.com>
2022-09-04 21:20:34 +0100

    13 lines just to be a little more readable

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import string, random, base64
 
 random_string = lambda length: ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(length))
-generate_token = lambda: base64.b64encode(str(random.randint(100000000000000000, 999999999999999999)).encode()).decode() + "." + random_string(5) + "." + random_string(38)
+get_user_id = lambda: base64.b64encode(str(random.randint(100000000000000000, 999999999999999999)).encode()).decode()
+generate_token = lambda: get_user_id() + "." + random_string(5) + "." + random_string(38)
 
 if __name__ == "__main__":
     amount = int(input("Amount to generate: "))
