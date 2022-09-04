bc7ef747a7f28cb15ff359c59cf78f27fe67a499
Ben Tettmar <bentettmar@yahoo.com>
2022-09-04 21:14:48 +0100

    bro im unstoppable

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import string, random, base64
 
-random_string = lambda length: ''.join(random.choice(string.ascii_letters) for _ in range(length))
-
-def generate_token():
-    return base64.b64encode(str(random.randint(100000000000000000, 999999999999999999)).encode()).decode() + "." + random_string(5) + "." + random_string(38)
-
 if __name__ == "__main__":
+    random_string = lambda length: ''.join(random.choice(string.ascii_letters) for _ in range(length))
+    generate_token = lambda: base64.b64encode(str(random.randint(100000000000000000, 999999999999999999)).encode()).decode() + "." + random_string(5) + "." + random_string(38)
+
     amount = int(input("Amount to generate: "))
     accounts = [generate_token() for _ in range(amount)]
 
