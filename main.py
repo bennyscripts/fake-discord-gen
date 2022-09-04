be91b6d70533284d46f4537a1ad087dbe4d9e390
Ben Tettmar <bentettmar@yahoo.com>
2022-09-04 21:09:40 +0100

    17 lines

--- main.py
+++ main.py
@@ -7,13 +7,11 @@ def generate_token():
     encoded_user_id = base64.b64encode(str(user_id).encode()).decode()
     return encoded_user_id + "." + random_string(5) + "." + random_string(38)
 
-amount = int(input("Amount to generate: "))
-accounts = []
-for _ in range(amount):
-    accounts.append(generate_token())
+if __name__ == "__main__":
+    amount = int(input("Amount to generate: "))
+    accounts = [generate_token() for _ in range(amount)]
 
-save_to_file = input("Save to file? (y/n): ")
-if save_to_file == "y":
-    with open("accounts.txt", "w") as f:
-        for account in accounts:
-            f.write(f"{account.token}\n")
+    save_to_file = input("Save to file? (y/n): ")
+    if save_to_file == "y":
+        with open("accounts.txt", "w") as f:
+            f.write("\n".join(accounts))
